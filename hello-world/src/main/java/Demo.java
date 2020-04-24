1e1e0020542757c7ae36f86452042788b514ff97
chandrabiji <chandramca04@gmail.com>
2020-04-24 22:31:45 +0530

    first commit

---
+++ hello-world/src/main/java/Demo.java
@@ -0,0 +1,12 @@
+
+public class Demo {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=10;++i)
+		{
+			System.out.println("Welcome to git maven project "+i);
+		}
+       System.out.println("Finish loop");
+	}
+
+}
